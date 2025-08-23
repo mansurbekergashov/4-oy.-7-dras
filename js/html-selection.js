361ec9775e6c6dae0ec8e9cd79a57aeb8c70162e
mansurbek <ergashovmansurbek5577@gmail.com>
2025-08-23 10:25:10 +0500

    don-don-ziki

---
+++ js/html-selection.js
@@ -0,0 +1,9 @@
+const elHands = document.querySelectorAll(".hand");
+const elPlayer = document.querySelector(".player");
+const elAI = document.querySelector(".ai");
+const elStatusZone = document.getElementById("statusZone");
+const elChooseZone = document.getElementById("chooseZone");
+const elRefreshGameButton = document.getElementById("refreshGameButton");
+const elStatusText = document.getElementById("statusText");
+
+export { elHands, elPlayer, elStatusZone, elChooseZone, elRefreshGameButton, elAI, elStatusText };
---
+++ js/main.js
@@ -0,0 +1,31 @@
+import { aiChoose } from "./ai-choose.js";
+import { checkWinner } from "./check-winner.js";
+import { elHands, elPlayer, elRefreshGameButton, elAI, elStatusText } from "./html-selection.js";
+import { switchZone } from "./switch-zone.js";
+
+elHands.forEach((el) => {
+    el.addEventListener("click", (e) => {
+        const player = e.target.alt;
+        const playerSrc = e.target.src;
+        elPlayer.src = playerSrc;
+        switchZone(true);
+        setTimeout(() => {
+            const ai = aiChoose();
+            // const winner = checkWinner(ai, player);
+            // elAI.src = `./images/${ai}.svg`;
+            // elStatusText.textContent = winner;
+            console.log(ai);
+            
+
+        }, 1000);
+
+
+
+
+    });
+});
+
+
+elRefreshGameButton.addEventListener("click", () => {
+    switchZone(false)
+})
